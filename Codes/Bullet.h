970b42cd31f456d8f6594904b985b03faeb7d64b
FS <1737151705@qq.com>
2026-05-29 19:38:31 +0800

    Bullet搭建

--- Codes/Bullet.h
+++ Codes/Bullet.h
@@ -2,8 +2,16 @@
 class Bullet:public GameObject{
     // 子弹类
     // public继承：位置 移动 碰撞箱
-    // 子类属性：子弹伤害 子弹种类（与武器有关）
+    // 子类属性：子弹伤害 子弹种类（与武器有关，单独放到不同子类中）
     // 对外接口：子弹攻击到目标实体
+    protected:
+        int damage;
+    public:
+        void setDamage(const int D);
+        void attack();
 };
+void Bullet::setDamage(const int D){
+    damage = D;
+}
 
 class bulletExample:public Bullet{};
